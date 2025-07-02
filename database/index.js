3985ac8f589f963aeff9565d90b51274c714b8a0
hailia sommerville <hailiassommerville@gmail.com>
2025-07-01 22:43:26 -0400

    get student by id

--- database/index.js
+++ database/index.js
@@ -1,7 +1,10 @@
 const db = require("./db");
-const Duck = require("./duck");
+//const Duck = require("./duck");
+const Students = require("./students");
+const campus = require("./campus");
 
 module.exports = {
   db,
-  Duck,
+  Students,
+  campus
 };
