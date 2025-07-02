3e984d92bbda2fe35cda7b1b20d39f07854a124e
roselyn5554 <r.marte445@gmail.com>
2025-07-02 10:48:45 -0400

    campus routing

---
+++ api/campus.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const { Campus } = require("../database");
+
+
+      //async for await
+      //post creating a new campus
+router.post("/", async (req, res) => {
+
+   try{  
+    const camp = req.body;
+    await Campus.create(camp);
+    res.sendStatus(201);
+   }
+   catch(error) {
+   res.sendStatus(500);
+   }
+
+
+});
+
+// GET all ducks = retriveing info
+router.get("/", async (req, res) => {
+try{  
+    const camp = await Campus.findAll();
+    res.send(camp);
+   }
+   catch(error) {
+   res.sendStatus(500);
+   }
+
+});
+
+router.get("/:id", async (req, res) => {
+try{  
+    const camp = await  Campus.findByPk(req.params.id, {
+      include: Students,
+      
+    });
+    res.send(camp);
+   }
+   catch(error) {
+   res.sendStatus(500);
+   }
+
+});
+
+router.delete("/:id", async (req, res) => {
+try{  
+    const camp = Number(req.params.id);
+    const deleteCamp = await Campus.destroy({
+      where: {id: camp}
+    });
+   }
+   catch(error) {
+   res.sendStatus(500);
+   }
+
+});
+
+
+
+module.exports = router;
